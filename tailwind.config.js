99eacc7e216a7e07ab34d25f7ff3b126bc8f658f
Juano <juanjosemunozva@gmail.com>
2023-09-14 20:27:23 -0500

    Citas spring-boot

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,55 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      
       animation: {
         fadeIn: "fadeIn 2s ease-in-out",
-        zoomIn: "zoomIn 2s ease-in-out",
+        fadeOut: "fadeIn 0.5s ease-in-out",
+        zoomIn: "zoomIn 0.5s ease-in-out",
+        zoomOut: "zoomOut 0.5s ease-in-out",
+        leaving: "leaving 1s ease-in-out",
+        entrando: "entrando 1s ease-in-out",
       },
       keyframes: {
+        leaving: {
+          "0%": {
+            top: 0,
+            right: 0,
+          },
+          "100%": {
+            top: 0,
+            right: "-20rem",
+          },
+        },
+        entrando: {
+          "0%": {
+            top: 0,
+            right: "-20rem",
+          },
+          "100%": {
+            top: 0,
+            right: "0",
+          },
+        },
         fadeIn: {
           "0%": {
             opacity: 0,
           },
           "100%": { opacity: 100 },
         },
+        fadeOut: {
+          "0%": {
+            opacity: 100,
+          },
+          "100%": { opacity: 0 },
+        },
         zoomIn: {
           "0%": { transform: "scale(1)" },
           "100%": { transform: "scale(0)" },
+        },
+        zoomOut: {
+          "0%": { transform: "scale(0)" },
+          "100%": { transform: "scale(1)" },
         },
       },
     },
